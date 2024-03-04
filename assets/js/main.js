bc4db45cdac61c7f1e54436b02d99d194308996a
titohajili <maqatoto963@gmail.com>
2024-03-04 21:14:26 +0400

    Change Background Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,14 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=========== ADD BLUR TO HEADER =========*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', blurHeader)
