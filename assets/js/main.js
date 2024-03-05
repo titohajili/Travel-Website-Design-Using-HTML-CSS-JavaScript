aa532e018433dde7c86d2d232bd3991fa3fa40e7
titohajili <maqatoto963@gmail.com>
2024-03-05 12:38:32 +0400

    Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,4 @@ const blurHeader = () =>{
                        : header.classList.remove('blur-header')
 }
 
-window.addEventListener('scroll', blurHeader)
+window.addEventListener('scroll', blurHeader);
