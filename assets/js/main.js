067ae76dbdd2f3144b74a51d501766dc3a27a164
titohajili <maqatoto963@gmail.com>
2024-03-05 15:54:08 +0400

    Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,36 @@ const blurHeader = () =>{
 }
 
 window.addEventListener('scroll', blurHeader);
+/*========== SHOW SCROLL UP ==============*/
+
+const scrollUp = () =>{
+
+const scrollUp = document.getElementById('scroll-up')
+this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/*============ SCROLL SCTIONS ACTIVE LINK =========*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
